bb10903191c91420e376b3517ebc415ba82f544f
AD\20210059 <shijjjung@gmail.com>
2021-12-24 11:03:17 +0800

    feat: 整理程式碼

--- app.py
+++ app.py
@@ -35,110 +35,97 @@ def callback():
             abort(400)
 
         return "OK"
-
-
+# 發問出席統計按鈕
+def echoJoinButtons(data, reply_token):
+    line_bot_api.reply_message(  # 回復傳入的訊息文字
+        reply_token,
+        TemplateSendMessage(
+            alt_text='Buttons template',
+            template=ButtonsTemplate(
+                title='簽到',
+                text='各位隊員家人們～出席{data}團練者，請於以下回覆，以利掌握人數，謝謝。'.format(data=data),
+                actions=[
+                    PostbackTemplateAction(
+                        label='我會到',
+                        text='我會到',
+                        data='A&'+data
+                    ),
+                    PostbackTemplateAction(
+                        label='我會晚到',
+                        text='我會晚到',
+                        data='B&'+data
+                    ),
+                ]
+            )
+        )
+    )
+def echoMembersJoined(connection, token, reg_col1):
+    with connection.cursor() as cursor:
+        select_query = """select `reg_name2` from Registration where reg_part = 1 and `reg_col1` = '{datestr}'""".format(datestr=reg_col1)
+        cursor.execute(select_query)
+        records = cursor.fetchall()
+        i = 1
+        txt = ""
+        for row in records:
+            txt = txt + "{i}. {name}\n".format(i=i, name=row[0])
+            i = i + 1
+        line_bot_api.reply_message(  # 回復傳入的訊息文字
+            token,
+            TextSendMessage(text=txt)
+        )
+# 回應參加
+def doRegister(profile, event, postdata):
+    try:
+        sp_sql = """SELECT SP_UPDATEMEMBER('{u_id}','{n}');""".format(u_id = event.source.user_id, n=profile.display_name)
+        date =  postdata.split('&')[-1]
+        connection=pymysql.connect(host=os.environ.get("MYSQL_HOST"),user=os.environ.get("USER"),password=os.environ.get("PW"),db='message',charset='utf8mb4')
+        with connection.cursor() as cursor:
+            sql= """
+            INSERT INTO `Registration`(`reg_name`,`reg_name2`,`reg_part`,`reg_col1`,`reg_col2`) 
+            SELECT '{name}', (select `m_id`from `Member` where `m_uid`='{u_id}' LIMIT 1), 1, '{datestr}','{memo}' from `Member` WHERE not EXISTS 
+            (SELECT 1 FROM `Registration` WHERE `reg_col1` = '{datestr}' and `reg_name2` = (select `m_id` from `Member` WHERE 
+            `m_uid` = '{u_id}')) AND `m_uid` = '{u_id}' LIMIT 1;""".format(
+                name=profile.display_name, 
+                datestr=date,
+                memo=postdata,
+                u_id = event.source.user_id
+            )
+            cursor.execute(sp_sql)
+            cursor.execute(sql)
+            connection.commit()
+        echoMembersJoined(connection=connection, token=event.reply_token, reg_col1=date)
+        echoJoinButtons(date, event.reply_token )
+        connection.close()
+    except Exception as ex:
+        line_bot_api.reply_message(  # 回復傳入的訊息文字
+            event.reply_token,
+            TextSendMessage(text=str(ex))
+        )
 @handler.add(PostbackEvent)
 def handle_postback(event):
     profile = line_bot_api.get_profile(event.source.user_id)
     if event.postback.data[0:1] == "A" or event.postback.data[0:1] == "B":
+        doRegister(profile, event, event.postback.data)
         
-        try:
-            sp_sql = """SELECT SP_UPDATEMEMBER('{u_id}','{n}');""".format(u_id = event.source.user_id, n=profile.display_name)
-            date = event.postback.data.split('&')[-1]
-            connection=pymysql.connect(host=os.environ.get("MYSQL_HOST"),user=os.environ.get("USER"),password=os.environ.get("PW"),db='message',charset='utf8mb4')
-            with connection.cursor() as cursor:
-                sql= """INSERT INTO `Registration`
-                ( `reg_name`, `reg_name2`, `reg_part`, `reg_col1`, `reg_col2`) 
-                VALUES ('{name}', '{name2}', 1, '{datestr}', '{memo}')""".format(
-                    name=profile.display_name, 
-                    name2=profile.display_name,
-                    datestr=date,
-                    memo=event.postback.data
-                )
-                cursor.execute(sp_sql)
-                cursor.execute(sql)
-                connection.commit()
-            with connection.cursor() as cursor:
-                select_query = """select `reg_name2` from Registration where reg_part = 1 and `reg_col1` = '{datestr}'""".format(datestr=date)
-                cursor.execute(select_query)
-                records = cursor.fetchall()
-                i = 1
-                txt = ""
-                for row in records:
-                   txt = txt + "{i}. {name}\n".format(i=i, name=row[0])
-                   i = i + 1
-                line_bot_api.reply_message(  # 回復傳入的訊息文字
-                    event.reply_token,
-                    TextSendMessage(text=txt)
-                )
-            line_bot_api.reply_message(  # 回復傳入的訊息文字
-                event.reply_token,
-                TemplateSendMessage(
-                    alt_text='Buttons template',
-                    template=ButtonsTemplate(
-                        title='簽到',
-                        text='各位隊員家人們～出席{date}團練者，請於以下回覆，以利掌握人數，謝謝。'.format(date=date),
-                        actions=[
-                            PostbackTemplateAction(
-                                label='我會到',
-                                text='我會到',
-                                data='A&'+date
-                            ),
-                            PostbackTemplateAction(
-                                label='我會晚到',
-                                text='我會晚到',
-                                data='B&'+date
-                            ),
-                        ]
-                    )
-                )
-            )
-            connection.close()
-        except Exception as ex:
-            line_bot_api.reply_message(  # 回復傳入的訊息文字
-                event.reply_token,
-                TextSendMessage(text=str(ex))
-            )
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     get_message = event.message.text
-    if get_message.split(' ')[-1]=="團練":
+    if get_message.split(' ')[-1]=="團練" and event.source.user_id == 'Uf3ea47edfa9d6c08b8c14786d2fd043a':
         date = get_message.split(' ')[0]
-        line_bot_api.reply_message(  # 回復傳入的訊息文字
-            event.reply_token,
-            TemplateSendMessage(
-                alt_text='Buttons template',
-                template=ButtonsTemplate(
-                    title='簽到',
-                    text='各位隊員家人們～出席{date}團練者，請於以下回覆，以利掌握人數，謝謝。'.format(date=date),
-                    actions=[
-                        PostbackTemplateAction(
-                            label='我會到',
-                            text='我會到',
-                            data='A&'+date
-                        ),
-                        PostbackTemplateAction(
-                            label='我會晚到',
-                            text='我會晚到',
-                            data='B&'+date
-                        ),
-                    ]
-                )
-            )
-        )
-        return 
-    elif get_message in ['不出席','不會到']:
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='本月簽到次數{0},今年練習一共{1}次，出席率為：{2}'.format(str(3), str(5), str(int((3/5)*100))))
-        )
-    elif '出席' in get_message:
-        #簽到完成
-        profile = line_bot_api.get_profile(event.source.user_id)
-        line_bot_api.reply_message(  # 回復傳入的訊息文字
-            event.reply_token,
-            TextSendMessage(text=profile.display_name)
-        )
+        echoJoinButtons(date, event.reply_token)
+        return
+    # elif get_message in ['不出席','不會到']:
+    #     line_bot_api.reply_message(
+    #         event.reply_token,
+    #         TextSendMessage(text='本月簽到次數{0},今年練習一共{1}次，出席率為：{2}'.format(str(3), str(5), str(int((3/5)*100))))
+    #     )
+    # elif '出席' in get_message:
+    #     #簽到完成
+    #     profile = line_bot_api.get_profile(event.source.user_id)
+    #     line_bot_api.reply_message(  # 回復傳入的訊息文字
+    #         event.reply_token,
+    #         TextSendMessage(text=profile.display_name)
+    #     )
     # Send To Line
     # reply = TextSendMessage(text=f"{get_message}")
     # line_bot_api.reply_message(event.reply_token, reply)
